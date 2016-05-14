0e36b878467669dd62ade8a46204166b008fe7de
Pine Wu <octref@gmail.com>
2016-05-14 12:47:24 -0400

    Update photon program

--- photon/main.ino
+++ photon/main.ino
@@ -12,6 +12,8 @@ CRGB leds2[NUM_LEDS];
 CRGB snake = CRGB::Blue;
 CRGB target = CRGB::Red;
 CRGB black = CRGB::Black;
+CRGB special = CRGB::Orange;
+CRGB extra = CRGB::Purple;
 
 void setup() {
   FastLED.addLeds<WS2812, DATA_PIN>(leds, NUM_LEDS);
@@ -35,27 +37,34 @@ void loop() {
           leds[i] = black;
         } else if (c == '1') {
           leds[i] = snake;
-        } else {
+        } else if (c == '2'){
           leds[i] = target;
+        } else if (c == '3') {
+          leds[i] = special;
+        } else {
+          leds[i] = extra;
         }
       } else {
         if (c == '0') {
           leds2[i % 128] = black;
         } else if (c == '1') {
           leds2[i % 128] = snake;
-        } else {
+        } else if (c == '2') {
           leds2[i % 128] = target;
+        } else if (c == '3') {
+          leds2[i % 128] = special;
+        } else {
+          leds2[i % 128] = extra;
         }
       }
-
     }
 
     FastLED.show();
 
     int hor = analogRead(A0);
     int ver = analogRead(A1);
-    int spa = analogRead(A2);
-    Serial.printf("%d,%d,%d\n", hor, ver, spa);
+    int but = analogRead(A2);
+    Serial.printf("%d,%d,%d\n", hor, ver, but);
     delay(10);
   }
 }
